f37b04036d5c3b920d769527fa028152400db5e7
Amir Moualem <amouaem@gmail.com>
2024-08-06 18:45:13 +0300

    calcs

---
+++ calculator/calculator.py
@@ -0,0 +1,9 @@
+class Calculator:
+    def add(self, a: int, b: int):
+        return a + b
+    def sub(self, a: int, b: int):
+        return a - b
+    def mul(self, a: int, b: int):
+        return a * b
+    def div(self, a: int, b: int):
+        return a / b
